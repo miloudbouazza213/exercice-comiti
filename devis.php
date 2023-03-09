a674003613dd1a595e854b7ac570c1250958bab7
Miloud Bouazza <miloud.bouazza@etudiant.univ-lr.fr>
2023-03-09 15:30:02 +0100

    ajout du calcul du prix des section

--- devis.php
+++ devis.php
@@ -64,24 +64,61 @@ function calculerPrixTtcAnnuel($nbAdherents, $nbSections, $federation)
         $prixHTAdherents = $prixHTAdherents - ($prixHTAdherents*15/100);
     }
 
-    // prix TTC :
+    // prix TTC adherents :
     $prixTTCAdherents = $prixHTAdherents + ($prixHTAdherents * TAUX_TVA /100 );
 
+
+    //--------- calcul prix TTC des section sur 12 mois :
+    
+    $prixHTsections = 0;
+    $prixTTCsections = 0;
+    $nbSectionApayer = $nbSections;
+
+    # - une section est offerte si le club possède  plus de 1000 adhérents. 
+    if($nbAdherents>1000 ){
+        $nbSectionApayer--;
+    }
+    # - Fédération de Natation (“N”) -> 3 sections offertes 
+    if($federation =="N"){
+        $nbSectionApayer=$nbSectionApayer-3;
+    }
+
+    if($nbSectionApayer>0){
+        //calcul du prix HT des section sur 12 mois 
+        # 5€/section/mois HT
+        $prixHTsections = $nbSectionApayer * 5 * 12;
+
+        #- Fédération de Basketball (“B”) -> 30% de réduction sur le cout des sections
+        if($federation == "B"){
+            $prixHTsections = $prixHTsections - ($prixHTsections*30/100);
+        }
+    }
+
+        // prix TTC section :
+        $prixTTCsections = $prixHTsections + ($prixHTsections * TAUX_TVA /100 );
+
     //test
-    return $prixTTCAdherents;
+    return $prixTTCsections;
 
 }
 
 //test pour calcul prix adherents
-echo "pour 60 adherents attendu :144 , obetenu: ". calculerPrixTtcAnnuel(60,0,"autre") ."<br>";
-echo "pour 100 adherents attendu :144 , obetenu: ". calculerPrixTtcAnnuel(100,0,"autre") ."<br>";
-echo "pour 100 adherents et fed G attendu :122,40  , obetenu: ". calculerPrixTtcAnnuel(100,0,"G") ."<br>";
-echo "pour 101 adherents attendu :145.44 , obetenu: ". calculerPrixTtcAnnuel(101,0,"autre") ."<br>";
-echo "pour 503 adherents attendu :579.45 , obetenu: ". calculerPrixTtcAnnuel(503,0,"autre") ."<br>";
-echo "pour 3005 adherents attendu :4032 , obetenu: ". calculerPrixTtcAnnuel(3005,0,"autre") ."<br>";
-echo "pour 3005 adherents et fed G attendu :3427.2 , obetenu: ". calculerPrixTtcAnnuel(3005,0,"G") ."<br>";
-echo "pour 10001 adherents attendu :14400 , obetenu: ". calculerPrixTtcAnnuel(10001,0,"autre") ."<br>";
-echo "pour 10001 adherents et fed G attendu :12240 , obetenu: ". calculerPrixTtcAnnuel(10001,0,"G") ."<br>";
+// echo "pour 60 adherents attendu :144 , obetenu: ". calculerPrixTtcAnnuel(60,0,"autre") ."<br>";
+// echo "pour 100 adherents attendu :144 , obetenu: ". calculerPrixTtcAnnuel(100,0,"autre") ."<br>";
+// echo "pour 100 adherents et fed G attendu :122,40  , obetenu: ". calculerPrixTtcAnnuel(100,0,"G") ."<br>";
+// echo "pour 101 adherents attendu :145.44 , obetenu: ". calculerPrixTtcAnnuel(101,0,"autre") ."<br>";
+// echo "pour 503 adherents attendu :579.45 , obetenu: ". calculerPrixTtcAnnuel(503,0,"autre") ."<br>";
+// echo "pour 3005 adherents attendu :4032 , obetenu: ". calculerPrixTtcAnnuel(3005,0,"autre") ."<br>";
+// echo "pour 3005 adherents et fed G attendu :3427.2 , obetenu: ". calculerPrixTtcAnnuel(3005,0,"G") ."<br>";
+// echo "pour 10001 adherents attendu :14400 , obetenu: ". calculerPrixTtcAnnuel(10001,0,"autre") ."<br>";
+// echo "pour 10001 adherents et fed G attendu :12240 , obetenu: ". calculerPrixTtcAnnuel(10001,0,"G") ."<br>";
+
+//test calcul prix section 
+echo "pour 60 adherents 10 section ,prix section attendu :720 , obetenu: ". calculerPrixTtcAnnuel(60,10,"autre") ."<br>";
+echo "pour 1001 adherents (1section offerte) 10 section ,prix section attendu :648 , obetenu: ". calculerPrixTtcAnnuel(1001,10,"autre") ."<br>";
+echo "pour 60 adherents 10 section fed N ( 3 section off.) ,prix section attendu :504 , obetenu: ". calculerPrixTtcAnnuel(60,10,"N") ."<br>";
+echo "pour 60 adherents 15 section fed B ( -30%) ,prix section attendu :756 , obetenu: ". calculerPrixTtcAnnuel(60,15,"B") ."<br>";
+echo "pour 1001 adherents fed N (4section offerte) 3 section ,prix section attendu :0 , obetenu: ". calculerPrixTtcAnnuel(1001,3,"N") ."<br>";
 
 
 ?>
